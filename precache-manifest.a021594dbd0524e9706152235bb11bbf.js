4144fc289b8e00d9eb697731db9ef0f1eca38909
nzhannu <admamgain@yahoo.co.nz>
2023-01-22 21:33:46 +1300

    Updates

---
+++ precache-manifest.a021594dbd0524e9706152235bb11bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c41416785cf91d9b3a59",
+    "url": "/react-web/static/css/main.ce9e93ef.chunk.css"
+  },
+  {
+    "revision": "c41416785cf91d9b3a59",
+    "url": "/react-web/static/js/main.c4141678.chunk.js"
+  },
+  {
+    "revision": "de777e5fde807a0452ce",
+    "url": "/react-web/static/js/1.de777e5f.chunk.js"
+  },
+  {
+    "revision": "8356e5d61182ddabce48",
+    "url": "/react-web/static/js/2.8356e5d6.chunk.js"
+  },
+  {
+    "revision": "78c3c5f4e3b352f011b9",
+    "url": "/react-web/static/js/runtime~main.78c3c5f4.js"
+  },
+  {
+    "revision": "c0639b6577bc381669a76e63f3a1fd88",
+    "url": "/react-web/index.html"
+  }
+];
